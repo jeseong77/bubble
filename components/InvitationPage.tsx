ebd565adf355088d905a338f08d3af554c6798f0
calsdn <alsdnchoiii@gmail.com>
2025-09-10 22:26:32 +0900

    Update InvitationPage.tsx
    invitation page empty message moved up

--- components/InvitationPage.tsx
+++ components/InvitationPage.tsx
@@ -631,6 +631,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 32,
+    marginTop: -90,  // Move up by header height + 30px
   },
   emptyIconContainer: {
     marginBottom: 24,
